2b2806cee5a65d4fac6710802435de222f7f34ec
Lawrence <locaulker@gmail.com>
2017-01-25 19:05:53 -0500

    Completed merge-stream, gulp-newer and gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ sass = require('gulp-sass'),
 browserSync = require('browser-sync'),
 reload = browserSync.reload,
 autoprefixer = require('gulp-autoprefixer'),
+browserify = require('gulp-browserify'),
 clean = require('gulp-clean'),
-concat = require('gulp-concat');
+concat = require('gulp-concat'),
+merge = require('merge-stream'),
+newer = require('gulp-newer'),
+imagemin = require('gulp-imagemin');
 
 // PATHS
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
   htmlSource : 'src/*.html',
-  jsSource : 'src/js/**/*.js'
+  jsSource : 'src/js/**',
+  imgSource : 'src/img/**'
 }
 
 var APPPATH = {
   root: 'app/',
   css : 'app/css',
-  js : 'app/js'
+  js : 'app/js',
+  fonts : 'app/fonts',
+  img : 'app/img'
 }
 
 // Clean - Remove html files deleted in "src" from "app" folder also
@@ -33,18 +40,35 @@ gulp.task('clean-scripts', function() {
 
 //SASS Task - Compile SCSS Files to CSS Files
 gulp.task('sass', function() {
-  return gulp.src(SOURCEPATHS.sassSource)
+  var bootstrapCSS  = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+  // var sassFiles;
+  var sassFiles = gulp.src(SOURCEPATHS.sassSource)
     .pipe(autoprefixer())
-    .pipe(sass(
-        {
-          outputStyle: 'expanded'
-        }).on('error', sass.logError))
-    .pipe(gulp.dest(APPPATH.css));
+    .pipe(sass({ outputStyle: 'expanded' })
+    .on('error', sass.logError))
+    return merge(bootstrapCSS, sassFiles)
+      .pipe(concat('app.css'))
+      .pipe(gulp.dest(APPPATH.css));
+});
+
+// Image Tasks
+gulp.task('images', function() {
+  return gulp.src(SOURCEPATHS.imgSource)
+    .pipe(newer(APPPATH.img))
+    .pipe(imagemin())
+    .pipe(gulp.dest(APPPATH.img))
+});
+
+// Move BootStrap Fonts
+gulp.task('moveFonts', function() {
+  gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest(APPPATH.fonts));
 });
 
 gulp.task('scripts', ['clean-scripts'], function() {
   gulp.src(SOURCEPATHS.jsSource)
     .pipe(concat('main.js'))
+    .pipe(browserify())
     .pipe(gulp.dest(APPPATH.js))
 });
 
@@ -68,7 +92,7 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
   gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
